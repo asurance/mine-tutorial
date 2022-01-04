9cfc438d871f79b391654b3082a8fe35f57b25da
asurance <784672616@qq.com>
2022-01-04 18:38:40 +0800

    以tutorial_01初始化项目

---
+++ tutorial_02/src/main.rs
@@ -0,0 +1,29 @@
+use amethyst::{
+    prelude::*,
+    renderer::{types::DefaultBackend, RenderToWindow, RenderingBundle},
+    window::DisplayConfig,
+    GameDataBuilder,
+};
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+    let app_root = amethyst::utils::application_root_dir()?;
+    let assets_root = app_root.join("assets");
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new().with_plugin(
+            RenderToWindow::from_config(DisplayConfig {
+                title: "扫雷".to_string(),
+                dimensions: Some((800, 600)),
+                ..Default::default()
+            })
+            .with_clear([0.0, 0.0, 0.0, 1.0]),
+        ),
+    )?;
+    let mut game = Application::new(assets_root, Mine, game_data)?;
+    game.run();
+    Ok(())
+}
+
+struct Mine;
+
+impl SimpleState for Mine {}
